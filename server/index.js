ef756bcd686a18c3fd5c76e6eec0ece19e06b0db
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-06-26 14:52:47 +0600

    Server: add a post request for jwt

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser =require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin:['http://localhost:5173'],
+  credentials:true
+}))
+app.use(cookieParser())
 
 
 
@@ -25,6 +31,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // use jwt
+  app.post('/jwt', async(req,res) =>{
+    const user = req.body;
+    console.log(user);
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1h'})
+  
+    res
+    .cookie('token', token, {
+      httpOnly:true,
+      secure:false
+    })
+    .send({success:true})
+  })
     
     // connect to database 
     const database = client.db("gameWorldDB");
